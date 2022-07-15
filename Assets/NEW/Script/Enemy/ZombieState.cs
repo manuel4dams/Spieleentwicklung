fb5c792d424d075d645881319a0eae9ecd889dd8
adamsmanuel <adams5464@gmail.com>
2022-07-15 11:07:01 +0200

    draft zombie gamegraph

--- Assets/NEW/Script/Enemy/ZombieState.cs
+++ Assets/NEW/Script/Enemy/ZombieState.cs
@@ -1,7 +1,20 @@
+using GameGraph;
+using UnityEngine;
+
 namespace ScriptGG
 {
-    public class ZombieState
-    {
 
+    [GameGraph]
+    public class ZombieState : MonoBehaviour
+    {
+        [Header("Health")] //
+
+        [Header("Movement parameters")] //
+
+        [Header("Damage")] //
+
+        [Header("Visual")] //
+        public Material[] materials;
+        public Material material;
     }
 }
--- Assets/NEW/Script/Enemy/ZombieStateController.cs
+++ Assets/NEW/Script/Enemy/ZombieStateController.cs
@@ -1,7 +1,34 @@
+using GameGraph;
+using JetBrains.Annotations;
+using MyBox;
+using Unity.VisualScripting;
+using UnityEngine;
+
 namespace ScriptGG
 {
+    [GameGraph]
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
     public class ZombieStateController
     {
-        
+        // References
+        public ZombieState zombieState;
+        public SkinnedMeshRenderer skinnedMeshRenderer;
+
+
+        // Health
+
+        // Death
+
+        public void SetupMaterial()
+        {
+            if (zombieState.material)
+            {
+                skinnedMeshRenderer.material = zombieState.material;
+            }
+            else if (zombieState.materials.NotNullOrEmpty())
+            {
+                skinnedMeshRenderer.material = zombieState.materials[Random.Range(0, zombieState.materials.Length)];
+            }
+        }
     }
 }
